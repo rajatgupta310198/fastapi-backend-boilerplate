27d2665e3a08eec55433ac86002e5f0c25148abe
Rajat Gupta <rajat310198@outlook.com>
2022-12-05 22:16:05 +0530

    refactor

--- app/settings.py
+++ app/settings.py
@@ -1,16 +1,6 @@
 import os
 
 
-from dotenv import load_dotenv
-
- 
-
-def load_settings():
-    print("Loading settings...")
-    
-    load_dotenv(dotenv_path=os.getcwd() + "/.env")
-
-
 class AppConfigSettings:
     REDIS_HOST = os.getenv('REDIS_HOST')
     REDIS_PORT = os.getenv('REDIS_PORT')
